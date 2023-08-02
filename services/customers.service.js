353c2ef78bc2f5faf264acd0809f12082498a03c
JesusUriel24 <solonosabes@gmail.com>
2023-08-02 01:02:20 -0600

    implementando hashing

--- services/customers.service.js
+++ services/customers.service.js
@@ -1,5 +1,7 @@
 const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
 const { models } = require('../libs/sequelize');
+
 
 class CustomerService {
 
@@ -21,6 +23,14 @@ class CustomerService {
   }
 
   async create(data) {
+    const hash = await bcrypt.hash(data.user.password, 10);
+    const newData = {
+      ...data,
+      user: {
+        ...data.user,
+        password: hash
+      }
+    }
     const newCustomer = await models.Customer.create(data, {
       include: ['user']
     });
